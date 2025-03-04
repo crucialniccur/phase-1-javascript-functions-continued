456f173ec0b42c57bbd5345156c2f5c55701e942
Mati Joseph <crucialniccur@gmail.com>
2025-03-04 21:01:39 +0300

    comment out all to begin fresh

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-// code your solution here
+// // code your solution here
 // function saturdayFun(activity = "roller-skate") {
 //   console.log(`This Saturday, i want to ${activity}`);
+//   return "This Saturday, I want to bathe my dog!";
 // }
 
-// function expressions
-// const mondayWork = function (activity = "go to the office") {
-//   console.log(`This Monday, I will ${activity}`);
-// };
+// // function expressions
+// // const mondayWork = function (activity = "go to the office") {
+// //   console.log(`This Monday, I will ${activity}`);
+// // };
 
-// function wrapAdjective(iLike = "*") {
-//   const wrappedOne = function (random = "special") {
-//     return `'You are ${random}'`;
-//   };
-//   return wrappedOne;
-//   return iLike;
-// }
+// // function wrapAdjective(iLike = "*") {
+// //   const wrappedOne = function (random = "special") {
+// //     return `'You are ${random}'`;
+// //   };
+// //   return wrappedOne;
+// //   return iLike;
+// // }
 
-function saturdayFun(activity = "roller-skate") {
-  return `This Saturday, i want to ${activity}!`;
-}
+// // function saturdayFun(activity = "roller-skate") {
+// //   return `This Saturday, i want to ${activity}!`;
+// //   return "This Saturday, I want to bathe my dog!";
+// // }
