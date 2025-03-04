b637b4d0a69d74952fa233f20f681e0ec8a4c349
Mati Joseph <crucialniccur@gmail.com>
2025-03-04 20:40:34 +0300

    define the saturdayFun again

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 // code your solution here
+// function saturdayFun(activity = "roller-skate") {
+//   console.log(`This Saturday, i want to ${activity}`);
+// }
+
+// function expressions
+// const mondayWork = function (activity = "go to the office") {
+//   console.log(`This Monday, I will ${activity}`);
+// };
+
+// function wrapAdjective(iLike = "*") {
+//   const wrappedOne = function (random = "special") {
+//     return `'You are ${random}'`;
+//   };
+//   return wrappedOne;
+//   return iLike;
+// }
+
 function saturdayFun(activity = "roller-skate") {
   console.log(`This Saturday, i want to ${activity}`);
 }
